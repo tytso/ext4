9b90757b9276a48325075d976906a814ef6f1c81
Michael Haas <laga@laga.ath.cx>
2008-09-22 22:35:01 +0000

    UBUNTU: Add sec_perm-2.6.24.patch from AUFS - export security_inode_permission
    Signed-off-by: Michael Haas <laga@laga.ath.cx>
    Signed-off-by: Amit Kucheria <amit@canonical.com>

--- security/security.c
+++ security/security.c
@@ -416,6 +416,7 @@ int security_inode_rmdir(struct inode *dir, struct dentry *dentry,
 		return 0;
 	return security_ops->inode_rmdir(dir, dentry, mnt);
 }
+EXPORT_SYMBOL(security_inode_permission);
 
 int security_inode_mknod(struct inode *dir, struct dentry *dentry,
 			 struct vfsmount *mnt, int mode, dev_t dev)
