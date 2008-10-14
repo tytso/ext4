32e83b4fd7e333897ebde6f6c96596fb1172fe7e
Amit Kucheria <amit.kucheria@ubuntu.com>
2008-10-14 17:06:10 +0300

    UBUNTU: SAUCE: Adds support for COMPAL JHL90 webcam
    Needs to be pushed upstream.
    
    Signed-off-by: Joerg Schirottke <master@kanotix.com>
    Signed-off-by: Amit Kucheria <amit.kucheria@ubuntu.com>
    (cherry picked from commit 7acd7e01461577815ef8eb7ed2962332c4f5c47f)

--- drivers/media/video/uvc/uvc_driver.c
+++ drivers/media/video/uvc/uvc_driver.c
@@ -1984,6 +1984,15 @@ static struct usb_device_id uvc_ids[] = {
 	  .bInterfaceSubClass	= 1,
 	  .bInterfaceProtocol	= 0,
 	  .driver_info		= UVC_QUIRK_PROBE_MINMAX },
+	/* COMPAL JHL90 */
+        { .match_flags          = USB_DEVICE_ID_MATCH_DEVICE
+                                | USB_DEVICE_ID_MATCH_INT_INFO,
+          .idVendor             = 0x064e,
+          .idProduct            = 0xa115,
+          .bInterfaceClass      = USB_CLASS_VIDEO,
+          .bInterfaceSubClass   = 1,
+          .bInterfaceProtocol   = 0,
+          .driver_info          = UVC_QUIRK_PROBE_MINMAX },
 	/* Generic USB Video Class */
 	{ USB_INTERFACE_INFO(USB_CLASS_VIDEO, 1, 0) },
 	{}
