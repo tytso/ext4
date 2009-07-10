f60c856e64125debe319653917021bcb9077b2c2
Andy Whitcroft <apw@canonical.com>
2009-07-10 12:17:21 +0100

    UBUNTU: netbook -- reduce to a single flavour per architecture
    We only are interested in generating the basic desktop flavour for
    netbooks, we do not need server kernels.  Drop the others.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- debian/rules.d/amd64.mk
+++ debian/rules.d/amd64.mk
@@ -2,11 +2,11 @@ build_arch	= x86_64
 header_arch	= $(build_arch)
 asm_link	= x86
 defconfig	= defconfig
-flavours	= generic server
+flavours	= generic
 build_image	= bzImage
 kernel_file	= arch/$(build_arch)/boot/bzImage
 install_file	= vmlinuz
 
-server_sub	= virtual
+#server_sub	= virtual
 
 loader		= grub
