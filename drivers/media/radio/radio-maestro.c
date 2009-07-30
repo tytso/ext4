torvalds/linux

commit aa5e90af7d78d1711f8f4275ce3638817c0023dc

V4L/DVB (8783): v4l: add all missing video_device release callbacks
All drivers that call video_device_register where checked to see if they
set the release callback of struct video_device. Where that callback was
missing it was added.

Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

drivers/media/radio/radio-maestro.c
@@ -388,6 +388,7 @@ static struct video_device maestro_radio = {
 	.name           = "Maestro radio",
 	.fops           = &maestro_fops,
 	.ioctl_ops 	= &maestro_ioctl_ops,
+	.release	= video_device_release,
 };
 
 static int __devinit maestro_probe(struct pci_dev *pdev,

drivers/media/radio/radio-aimslab.c
@@ -420,6 +420,7 @@ static struct video_device rtrack_radio = {
 	.name		= "RadioTrack radio",
 	.fops           = &rtrack_fops,
 	.ioctl_ops 	= &rtrack_ioctl_ops,
+	.release	= video_device_release_empty,
 };
 
 static int __init rtrack_init(void)

drivers/media/radio/radio-aztech.c
@@ -381,9 +381,10 @@ static const struct v4l2_ioctl_ops aztech_ioctl_ops = {
 };
 
 static struct video_device aztech_radio = {
-	.name		    = "Aztech radio",
-	.fops               = &aztech_fops,
-	.ioctl_ops 	    = &aztech_ioctl_ops,
+	.name		= "Aztech radio",
+	.fops           = &aztech_fops,
+	.ioctl_ops 	= &aztech_ioctl_ops,
+	.release	= video_device_release_empty,
 };
 
 module_param_named(debug,aztech_radio.debug, int, 0644);

drivers/media/radio/radio-cadet.c
@@ -589,6 +589,7 @@ static struct video_device cadet_radio = {
 	.name		= "Cadet radio",
 	.fops           = &cadet_fops,
 	.ioctl_ops 	= &cadet_ioctl_ops,
+	.release	= video_device_release_empty,
 };
 
 #ifdef CONFIG_PNP

drivers/media/radio/radio-gemtek-pci.c
@@ -401,9 +401,10 @@ static const struct v4l2_ioctl_ops gemtek_pci_ioctl_ops = {
 };
 
 static struct video_device vdev_template = {
-	.name          = "Gemtek PCI Radio",
-	.fops          = &gemtek_pci_fops,
-	.ioctl_ops     = &gemtek_pci_ioctl_ops,
+	.name          	= "Gemtek PCI Radio",
+	.fops          	= &gemtek_pci_fops,
+	.ioctl_ops 	= &gemtek_pci_ioctl_ops,
+	.release	= video_device_release_empty,
 };
 
 static int __devinit gemtek_pci_probe( struct pci_dev *pci_dev, const struct pci_device_id *pci_id )

drivers/media/radio/radio-gemtek.c
@@ -581,9 +581,10 @@ static const struct v4l2_ioctl_ops gemtek_ioctl_ops = {
 };
 
 static struct video_device gemtek_radio = {
-	.name			= "GemTek Radio card",
-	.fops			= &gemtek_fops,
-	.ioctl_ops 		= &gemtek_ioctl_ops,
+	.name		= "GemTek Radio card",
+	.fops		= &gemtek_fops,
+	.ioctl_ops 	= &gemtek_ioctl_ops,
+	.release	= video_device_release_empty,
 };
 
 /*

drivers/media/radio/radio-maxiradio.c
@@ -402,9 +402,10 @@ static const struct v4l2_ioctl_ops maxiradio_ioctl_ops = {
 };
 
 static struct video_device maxiradio_radio = {
-	.name		    = "Maxi Radio FM2000 radio",
-	.fops               = &maxiradio_fops,
-	.ioctl_ops 	    = &maxiradio_ioctl_ops,
+	.name		= "Maxi Radio FM2000 radio",
+	.fops           = &maxiradio_fops,
+	.ioctl_ops 	= &maxiradio_ioctl_ops,
+	.release	= video_device_release_empty,
 };
 
 static int __devinit maxiradio_init_one(struct pci_dev *pdev, const struct pci_device_id *ent)

drivers/media/radio/radio-rtrack2.c
@@ -326,6 +326,7 @@ static struct video_device rtrack2_radio = {
 	.name		= "RadioTrack II radio",
 	.fops           = &rtrack2_fops,
 	.ioctl_ops 	= &rtrack2_ioctl_ops,
+	.release	= video_device_release_empty,
 };
 
 static int __init rtrack2_init(void)

tytso/ext4

commit cc4057b17be975a0f71116b005aea0cf3a2f2728

UBUNTU: SAUCE: radio-maestro: fix panics on probe failure
BugLink: https://bugs.launchpad.net/ubuntu/+bug/357724

Probe failures in maestro_probe cause a double call to
video_device_release():

 a) indirectly when calling video_unregister_device()
via dev->release(dev) in device_release()
 b) explicity in the error handling at label errfr1

The second call causes the oops. This bug was introduced
with commit aa5e90af7d78d1711f8f4275ce3638817c0023dc when the
release method was added to struct video_device maestro_radio
but the error handling was not updated to take this into
consideration.

This fix corrects the error handling path to avoid the
double release.

Signed-off-by: Colin Ian King <colin.king@canonical.com>
Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
Acked-by: Tim Gardner <tim.gardner@canonical.com>
Acked-by: Stefan Bader <stefan.bader@canonical.com>

drivers/media/radio/radio-maestro.c
@@ -422,7 +422,8 @@ static int __devinit maestro_probe(struct pci_dev *pdev,
 	retval = video_register_device(maestro_radio_inst, VFL_TYPE_RADIO, radio_nr);
 	if (retval) {
 		printk(KERN_ERR "can't register video device!\n");
-		goto errfr1;
+		video_device_release(maestro_radio_inst);
+		goto errfr;
 	}
 
 	if (!radio_power_on(radio_unit)) {
@@ -437,8 +438,6 @@ static int __devinit maestro_probe(struct pci_dev *pdev,
 	return 0;
 errunr:
 	video_unregister_device(maestro_radio_inst);
-errfr1:
-	video_device_release(maestro_radio_inst);
 errfr:
 	kfree(radio_unit);
 err: